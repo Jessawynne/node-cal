1dc53b3f8962a56c6bac0d9d1e0b505793052866
Jessawynne <jessawynne@gmail.com>
2016-01-26 12:13:42 -0600

    initial test integration

--- tests/test.js
+++ tests/test.js
@@ -1,10 +1,15 @@
 'use strict';
 
-var { expect } = require('chai');
+const { expect } = require('chai');
+//const cp = require('child_process');
+const { execSync } = require('child_process'); //since we are not using anything else from child_process
 
-describe('test suite', () => {
-  it('true should be true', () => {
-    expect(true).to.be.true;
+describe('cal', () => {
+  it('should handle the current month', () => {
+    const goal = execSync('cal').toString();
+    const output = execSync('./cal.js').toString();
+
+    expect(output).to.equal(goal);
   });
 });
 
