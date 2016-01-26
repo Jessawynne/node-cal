1f21ccc4eb011614609d0a1ae475698e9335db5b
Jessawynne <jessawynne@gmail.com>
2016-01-26 15:43:22 -0600

    tests for date, month, year

--- tests/test.js
+++ tests/test.js
@@ -5,12 +5,72 @@ const { expect } = require('chai');
 const { execSync } = require('child_process'); //since we are not using anything else from child_process
 
 describe('cal', () => {
-  it('should handle the current month', () => {
-    const goal = execSync('cal').toString();
-    const output = execSync('./cal.js').toString();
 
-    expect(output).to.equal(goal);
+  describe('CLI', () => {
+    it('should handle the current month', () => {
+      const goal = execSync('cal').toString();
+      const output = execSync('./cal.js').toString();
+
+      expect(output).to.equal(goal);
+    });
   });
+
+  describe("Zeller's congruence", () => {
+    const zellers = require('../lib/zellers.js');
+
+    describe('.modifiedMonth', () => {
+
+      it('return 13 for January', () => {
+        expect(zellers.modifiedMonth(1)).to.equal(13);
+      });
+
+      it('return 14 for February', () => {
+        expect(zellers.modifiedMonth(2)).to.equal(14);
+      });
+
+      it('return 3 for March', () => {
+        expect(zellers.modifiedMonth(3)).to.equal(3);
+      });
+
+    });
+
+    describe('.modifiedYear', () => {
+
+      it('returns 2014 for January 2015', () => {
+       expect(zellers.modifiedYear(2015, 1)).to.equal(2014);
+      });
+
+      it('returns 2015 for February 2016', () => {
+        expect(zellers.modifiedYear(2016, 2)).to.equal(2015);
+      });
+
+      it('returns 2017 for March 2017', () => {
+        expect(zellers.modifiedYear(2017, 3)).to.equal(2013);
+      });
+
+    });
+
+    describe('.getDay', () => {
+      it('returns 2 (Tuesday) for March 1, 2016', () => {
+        expect(zellers.getDay(2016, 3, 1)).to.equal(2);
+      });
+
+      it('returns 1 (Monday) for March 1, 2100', () => {
+        expect(zellers.getDay(2100, 3, 1)).to.equal(1);
+      });
+
+      it('returns 0 (Sunday) for March 2, 2200', () => {
+        expect(zellers.getDay(2200, 3, 2)).to.equal(0);
+      });
+
+      it('returns 4 (Thursday) for March 1, 2300', () => {
+        expect(zellers.getDay(2300, 3, 1)).to.equal(4);
+      });
+
+    });
+
+  });
+
 });
 
 
